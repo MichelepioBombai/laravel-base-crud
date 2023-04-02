92bbf692d4627af340593d0778820869c5739433
MichelepioBombai <michelebombai@icloud.com>
2023-04-02 11:21:53 +0200

    add database

---
+++ database/migrations/2023_04_02_090735_create_atrists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('author')->unique();
+            $table->text('poster')->nullable();
+            $table->string('title', 30);
+            $table->string('album', 50);
+            $table->integer('length');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+};
